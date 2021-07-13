3d66b30be0a0ec25acd70d97c2e942b7f3b6d934
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-07-13 15:50:36 +0700

    Membuat kelas costum button

---
+++ app/src/main/java/com/khoirullatif/customviewapp/MyButton.kt
@@ -0,0 +1,50 @@
+package com.khoirullatif.customviewapp
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+
+class MyButton : AppCompatButton {
+
+    private var colorText: Int = 0
+    private var enabledBgButton: Drawable? = null
+    private var disabledBgButon: Drawable? = null
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        setTextColor(colorText)
+        textSize = 12f
+        gravity = Gravity.CENTER
+        text = when {
+            isEnabled -> "Submit"
+            else -> "Isi dulu"
+        }
+        background = when {
+            isEnabled -> enabledBgButton
+            else -> disabledBgButon
+        }
+    }
+
+    private fun init() {
+        colorText = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBgButton = ResourcesCompat.getDrawable(resources, R.drawable.bg_button, null)
+        disabledBgButon = ResourcesCompat.getDrawable(resources, R.drawable.bg_button_disable, null)
+    }
+}
